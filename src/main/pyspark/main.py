aa46396f6ae6d7eb04be2bfc52e7dcc1e909758a
singhays <ayush.singh@adidas,.com>
2024-08-12 23:19:51 +0530

    CaseStudy

--- src/main/pyspark/main.py
+++ src/main/pyspark/main.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from Utils import Logger, ConfigLoader, FileImplicits
+from Utils import Logger, ConfigLoader
 from Context import parseArguments, Context
 from DataJob import LoadData
 from CalculateKPI import KPILoader
@@ -41,36 +41,8 @@ def main():
             logger.info("JobMode should be DataJob or CalculateKPI")
     finally:
         spark.stop()
-        # FileImplicits.deleteFileOrDirectory(context.config["sparkParameters"]["spark.local.dir"])
         logger.info("Application finished")
-
-
-
-
-
 
 
 if __name__=="__main__":
     main()
-    # spark = SparkSessionCreater(context.config["sparkParameters"]).createSpark()
-
-    # df.write.format('delta').mode('overwrite').partitionBy("Age").save("C:\\Users\\singhays\\Projects\\CaseStudy\\temp\\data")
-    # FileImplicits.deleteFileOrDirectory(context.config["sparkParameters"]["spark.local.dir"])
-    # o=DataframeImplicits.read(spark=spark, path="C:\\Users\\singhays\\Projects\\CaseStudy\\data\\OTIF_DATA_deivery_note.csv", format="csv", options={"header":"true","inferSchema":"true"})
-    # o.show()
-    # o.withColumn("date",to_date("Actual_Delivery_Date","dd-MM-yyyy")).show()
-    # o.printSchema()
-    # with open("C:\\Users\\singhays\\Projects\\CaseStudy\\src\\main\\tablesDDL\\Delivery.sql", "r") as f:
-    #     content = f.read()
-    # print(content)
-    # df = spark.sql(content)
-    # a = spark.sql("select * from delivery")
-    # a.show()
-    # o.withColumn("Actual_Delivery_Date",to_date("Actual_Delivery_Date","dd-MM-yyyy")).write.format("delta").mode('overwrite').partitionBy("Sales_Organization").save("C:\\Users\\singhays\\Projects\\CaseStudy\\tables\\delivery")
-    # a = spark.sql("select * from delivery")
-    # a.show()
-
-
-
-
-
